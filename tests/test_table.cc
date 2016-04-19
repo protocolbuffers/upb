215c956fba5de5e0663b793313bab951d02a6cbd
Josh Haberman <jhaberman@gmail.com>
2016-04-19 12:47:24 -0700

    Don't use UINTPTR_MAX or UINT64_MAX.

--- tests/test_table.cc
+++ tests/test_table.cc
@@ -574,10 +574,12 @@ void test_inttable(int32_t *keys, uint16_t num_entries, const char *desc) {
 void test_int64_max_value() {
   typedef upb::TypedIntTable<uint64_t> Table;
   Table table;
-  table.Insert(UINTPTR_MAX, UINT64_MAX);
-  std::pair<bool, uint64_t> found = table.Lookup(UINTPTR_MAX);
+  uintptr_t uintptr_max = (uintptr_t)-1;
+  uintptr_t uint64_max = (uint64_t)-1;
+  table.Insert(uintptr_max, uint64_max);
+  std::pair<bool, uint64_t> found = table.Lookup(uintptr_max);
   ASSERT(found.first);
-  ASSERT(found.second == UINT64_MAX);
+  ASSERT(found.second == uint64_max);
 }
 
 int32_t *get_contiguous_keys(int32_t num) {
