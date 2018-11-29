2d8da905b65eac34e06336d8d2b426c6959f7215
Bo Yang <teboring@google.com>
2018-11-28 16:35:48 -0800

    Debug

--- kokoro/ubuntu/build.sh
+++ kokoro/ubuntu/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $0)/../..
-bazel build :conformance_proto_upb_srcs.cc
-# bazel test :all
-ls -R
+# bazel build :conformance_proto_upb_srcs.cc
+bazel test :all
+ls bazel-bin/google/protobuf
+ls bazel-out/k8-fastbuild/bin/google/protobuf/
