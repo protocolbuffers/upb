7033c656c78543b09f58886f97cf3b658fd7dd30
Bo Yang <teboring@google.com>
2018-11-30 13:12:14 -0800

    Check bazel version

--- kokoro/ubuntu/build.sh
+++ kokoro/ubuntu/build.sh
@@ -3,8 +3,4 @@
 cd $(dirname $0)/../..
 # bazel build :conformance_proto_upb_srcs.cc
 bazel test :all
-ls -R
-ls bazel-bin/google/protobuf
-ls bazel-out/k8-fastbuild/bin/google/protobuf/
-ls -R bazel-bin
-ls -R bazel-out/k8-fastbuild/bin
+bazel version
