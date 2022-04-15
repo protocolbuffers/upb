104d5d8f1d8c7fc77b30ccf34abefff5f166d3fb
Protobuf Team <no-reply@google.com>
2022-04-15 15:02:48 -0700

    Update mini table accessors Part2 (repeated fields). Introduces upb_FieldValue for array accessor api.
    PiperOrigin-RevId: 442103970

--- upb/mini_table_accessors.c
+++ upb/mini_table_accessors.c
@@ -28,13 +28,12 @@
 #include "upb/mini_table_accessors.h"
 
 #include "upb/mini_table.h"
-#include "upb/mini_table_accessors_internal.h"
 #include "upb/msg_internal.h"
 
 // Must be last.
 #include "upb/port_def.inc"
 
-size_t upb_MiniTable_Field_GetSize(const upb_MiniTable_Field* f) {
+size_t upb_MiniTable_get_field_size(const upb_MiniTable_Field* f) {
   static unsigned char sizes[] = {
       0,                      /* 0 */
       8,                      /* kUpb_FieldType_Double */
@@ -57,11 +56,6 @@ size_t upb_MiniTable_Field_GetSize(const upb_MiniTable_Field* f) {
       8,                      /* kUpb_FieldType_SInt64 */
   };
   return upb_IsRepeatedOrMap(f) ? sizeof(void*) : sizes[f->descriptortype];
-}
-
-UPB_INLINE upb_Message* upb_MiniTable_GetMessage(
-    const upb_Message* msg, const upb_MiniTable_Field* field) {
-  return UPB_PTR_AT(msg, field->offset, upb_Message);
 }
 
 bool upb_MiniTable_HasField(const upb_Message* msg,
@@ -87,5 +81,106 @@ void upb_MiniTable_ClearField(upb_Message* msg,
     if (*oneof_case != field->number) return;
     *oneof_case = 0;
   }
-  memset(mem, 0, upb_MiniTable_Field_GetSize(field));
+  memset(mem, 0, upb_MiniTable_get_field_size(field));
+}
+
+static const char _upb_CTypeo_sizelg2[12] = {
+    0,
+    0,              /* kUpb_CType_Bool */
+    2,              /* kUpb_CType_Float */
+    2,              /* kUpb_CType_Int32 */
+    2,              /* kUpb_CType_UInt32 */
+    2,              /* kUpb_CType_Enum */
+    UPB_SIZE(2, 3), /* kUpb_CType_Message */
+    3,              /* kUpb_CType_Double */
+    3,              /* kUpb_CType_Int64 */
+    3,              /* kUpb_CType_UInt64 */
+    UPB_SIZE(3, 4), /* kUpb_CType_String */
+    UPB_SIZE(3, 4), /* kUpb_CType_Bytes */
+};
+
+upb_CType upb_MiniTable_Field_CType(const upb_MiniTable_Field* f) {
+  switch (f->descriptortype) {
+    case kUpb_FieldType_Double:
+      return kUpb_CType_Double;
+    case kUpb_FieldType_Float:
+      return kUpb_CType_Float;
+    case kUpb_FieldType_Int64:
+    case kUpb_FieldType_SInt64:
+    case kUpb_FieldType_SFixed64:
+      return kUpb_CType_Int64;
+    case kUpb_FieldType_Int32:
+    case kUpb_FieldType_SFixed32:
+    case kUpb_FieldType_SInt32:
+      return kUpb_CType_Int32;
+    case kUpb_FieldType_UInt64:
+    case kUpb_FieldType_Fixed64:
+      return kUpb_CType_UInt64;
+    case kUpb_FieldType_UInt32:
+    case kUpb_FieldType_Fixed32:
+      return kUpb_CType_UInt32;
+    case kUpb_FieldType_Enum:
+      return kUpb_CType_Enum;
+    case kUpb_FieldType_Bool:
+      return kUpb_CType_Bool;
+    case kUpb_FieldType_String:
+      return kUpb_CType_String;
+    case kUpb_FieldType_Bytes:
+      return kUpb_CType_Bytes;
+    case kUpb_FieldType_Group:
+    case kUpb_FieldType_Message:
+      return kUpb_CType_Message;
+  }
+  UPB_UNREACHABLE();
+}
+
+// Resizes storage for repeated fields.
+upb_RepeatedFieldArray* upb_MiniTable_ResizeArray(
+    upb_Message* msg, const upb_MiniTable_Field* field, size_t new_size,
+    upb_Arena* arena) {
+  upb_Array* array_val = *UPB_PTR_AT(msg, field->offset, upb_Array*);
+  if (!array_val) {
+    array_val = _upb_Array_New(
+        arena, new_size, _upb_CTypeo_sizelg2[upb_MiniTable_Field_CType(field)]);
+    if (!array_val) {
+      return NULL;
+    }
+    *UPB_PTR_AT(msg, field->offset, upb_Array*) = array_val;
+  } else {
+    if (!_upb_Array_Resize(array_val, new_size, arena)) {
+      return NULL;
+    }
+  }
+  return (upb_RepeatedFieldArray*)array_val;
+}
+
+void upb_MiniTable_SetArrayValue(upb_RepeatedFieldArray* array, size_t index,
+                                 const upb_FieldValue* value) {
+  upb_Array* arr = (upb_Array*)array;
+  char* data = _upb_array_ptr(arr);
+  int lg2 = arr->data & 7;
+  UPB_ASSERT(index < arr->len);
+  memcpy(data + (index << lg2), value, 1 << lg2);
+}
+
+upb_FieldValue upb_MiniTable_GetArrayValue(const upb_RepeatedFieldArray* array,
+                                           size_t index) {
+  upb_FieldValue ret;
+  upb_Array* arr = (upb_Array*)array;
+  const char* data = (const char*)_upb_array_constptr(arr);
+  int lg2 = arr->data & 7;
+  UPB_ASSERT(index < arr->len);
+  memcpy(&ret, data + (index << lg2), 1 << lg2);
+  return ret;
+}
+
+upb_MutableFieldValue upb_MiniTable_GetMutableArrayValue(
+    const upb_RepeatedFieldArray* array, size_t index) {
+  upb_MutableFieldValue ret;
+  upb_Array* arr = (upb_Array*)array;
+  char* data = (char*)_upb_array_ptr(arr);
+  int lg2 = arr->data & 7;
+  UPB_ASSERT(index < arr->len);
+  memcpy(&ret, data + (index << lg2), 1 << lg2);
+  return ret;
 }
--- upb/mini_table_accessors_test.cc
+++ upb/mini_table_accessors_test.cc
@@ -42,14 +42,26 @@ namespace {
 
 // Proto Field numbers used for reflective access.
 const uint32_t kFieldOptionalInt32 = 1;
+const uint32_t kFieldOptionalInt64 = 2;
+const uint32_t kFieldOptionalUInt32 = 3;
+const uint32_t kFieldOptionalUInt64 = 4;
 const uint32_t kFieldOptionalBool = 13;
 const uint32_t kFieldOptionalString = 14;
 const uint32_t kFieldOptionalNestedMessage = 18;
+const uint32_t kFieldOptionalRepeatedInt32 = 31;
+const uint32_t kFieldOptionalNestedMessageA = 1;
 const uint32_t kFieldOptionalOneOfUInt32 = 111;
 const uint32_t kFieldOptionalOneOfString = 113;
 
-const char kTestStr1[] = "Hello";
+const char kTestStr1[] = "Hello1";
+const char kTestStr2[] = "Hello2";
 const int32_t kTestInt32 = 567;
+const int32_t kTestUInt32 = 0xF1234567;
+
+const upb_MiniTable_Field* find_proto3_field(int field_number) {
+  return upb_MiniTable_FindFieldByNumber(
+      &protobuf_test_messages_proto3_TestAllTypesProto3_msginit, field_number);
+}
 
 const upb_MiniTable_Field* find_proto2_field(int field_number) {
   return upb_MiniTable_FindFieldByNumber(
@@ -148,7 +160,163 @@ TEST(GeneratedCode, ScalarsProto2) {
       kTestInt32,
       protobuf_test_messages_proto2_TestAllTypesProto2_optional_int32(msg));
 
+  const upb_MiniTable_Field* optional_uint32_field =
+      find_proto2_field(kFieldOptionalUInt32);
+
+  EXPECT_EQ(
+      0, protobuf_test_messages_proto2_TestAllTypesProto2_optional_uint32(msg));
+  EXPECT_EQ(0, upb_MiniTable_GetUInt32(msg, optional_uint32_field));
+  upb_MiniTable_SetUInt32(msg, optional_uint32_field, kTestUInt32);
+  EXPECT_EQ(kTestUInt32, upb_MiniTable_GetUInt32(msg, optional_uint32_field));
+  EXPECT_EQ(
+      kTestUInt32,
+      protobuf_test_messages_proto2_TestAllTypesProto2_optional_uint32(msg));
+
   upb_Arena_Free(arena);
+}
+
+TEST(GeneratedCode, Strings) {
+  upb_Arena* arena = upb_Arena_New();
+  protobuf_test_messages_proto2_TestAllTypesProto2* msg =
+      protobuf_test_messages_proto2_TestAllTypesProto2_new(arena);
+
+  const upb_MiniTable_Field* optional_string_field =
+      find_proto2_field(kFieldOptionalString);
+
+  // Test default.
+  EXPECT_EQ(false, upb_MiniTable_HasField(msg, optional_string_field));
+  // Test read after write using C.
+  protobuf_test_messages_proto2_TestAllTypesProto2_set_optional_string(
+      msg, upb_StringView_FromString(kTestStr1));
+  EXPECT_EQ(true, upb_MiniTable_HasField(msg, optional_string_field));
+  upb_StringView value = upb_MiniTable_GetString(msg, optional_string_field);
+  std::string read_value = std::string(value.data, value.size);
+  EXPECT_EQ(kTestStr1, read_value);
+  // Clear.
+  upb_MiniTable_ClearField(msg, optional_string_field);
+  EXPECT_EQ(false, upb_MiniTable_HasField(msg, optional_string_field));
+  EXPECT_EQ(
+      false,
+      protobuf_test_messages_proto2_TestAllTypesProto2_has_optional_string(
+          msg));
+  upb_MiniTable_SetString(msg, optional_string_field,
+                          upb_StringView_FromString(kTestStr2));
+  EXPECT_EQ(true, upb_MiniTable_HasField(msg, optional_string_field));
+  EXPECT_EQ(
+      true,
+      protobuf_test_messages_proto2_TestAllTypesProto2_has_optional_string(
+          msg));
+  value = protobuf_test_messages_proto2_TestAllTypesProto2_optional_string(msg);
+  read_value = std::string(value.data, value.size);
+  EXPECT_EQ(kTestStr2, read_value);
+
+  upb_Arena_Free(arena);
+}
+
+TEST(GeneratedCode, SubMessage) {
+  upb_Arena* arena = upb_Arena_New();
+  protobuf_test_messages_proto2_TestAllTypesProto2* msg =
+      protobuf_test_messages_proto2_TestAllTypesProto2_new(arena);
+
+  const upb_MiniTable_Field* optional_message_field =
+      find_proto2_field(kFieldOptionalNestedMessage);
+
+  const upb_Message* test_message =
+      upb_MiniTable_GetMessage(msg, optional_message_field);
+  EXPECT_EQ(NULL, test_message);
+
+  EXPECT_EQ(false, upb_MiniTable_HasField(msg, optional_message_field));
+
+  // Get mutable using C API.
+  protobuf_test_messages_proto2_TestAllTypesProto2_NestedMessage* nested_message =
+      protobuf_test_messages_proto2_TestAllTypesProto2_mutable_optional_nested_message(
+          msg, arena);
+  EXPECT_EQ(true, nested_message != nullptr);
+  EXPECT_EQ(true, upb_MiniTable_HasField(msg, optional_message_field));
+  protobuf_test_messages_proto2_TestAllTypesProto2_NestedMessage_set_a(
+      nested_message, 5);
+
+  // Read back using mini table API.
+  const upb_Message* sub_message =
+      upb_MiniTable_GetMessage(msg, optional_message_field);
+  EXPECT_EQ(true, sub_message != NULL);
+
+  const upb_MiniTable_Field* nested_message_a_field =
+      upb_MiniTable_FindFieldByNumber(
+          &protobuf_test_messages_proto2_TestAllTypesProto2_NestedMessage_msginit,
+          kFieldOptionalNestedMessageA);
+  EXPECT_EQ(5, upb_MiniTable_GetInt32(sub_message, nested_message_a_field));
+
+  upb_MiniTable_ClearField(msg, optional_message_field);
+  EXPECT_EQ(
+      NULL,
+      protobuf_test_messages_proto2_TestAllTypesProto2_optional_nested_message(
+          msg));
+  EXPECT_EQ(false, upb_MiniTable_HasField(msg, optional_message_field));
+
+  upb_Message* new_nested_message =
+      protobuf_test_messages_proto2_TestAllTypesProto2_NestedMessage_new(arena);
+  upb_MiniTable_SetInt32(new_nested_message, nested_message_a_field, 123);
+  upb_MiniTable_SetMessage(msg, optional_message_field, new_nested_message);
+
+  upb_Message* mutable_message = upb_MiniTable_GetMutableMessage(
+      msg, &protobuf_test_messages_proto2_TestAllTypesProto2_msginit,
+      optional_message_field, arena);
+  EXPECT_EQ(
+      true,
+      protobuf_test_messages_proto2_TestAllTypesProto2_optional_nested_message(
+          msg) != NULL);
+  EXPECT_EQ(true, upb_MiniTable_HasField(msg, optional_message_field));
+  EXPECT_EQ(123,
+            upb_MiniTable_GetInt32(mutable_message, nested_message_a_field));
+}
+
+TEST(GeneratedCode, RepeatedScalar) {
+  upb_Arena* arena = upb_Arena_New();
+  protobuf_test_messages_proto2_TestAllTypesProto2* msg =
+      protobuf_test_messages_proto2_TestAllTypesProto2_new(arena);
+
+  const upb_MiniTable_Field* repeated_int32_field =
+      find_proto2_field(kFieldOptionalRepeatedInt32);
+
+  size_t len;
+  const int32_t* arr =
+      protobuf_test_messages_proto2_TestAllTypesProto2_repeated_int32(msg,
+                                                                      &len);
+  // Test Get/Set Array values, validate with C API.
+  EXPECT_EQ(0, len);
+  EXPECT_EQ(NULL, arr);
+  EXPECT_EQ(NULL, upb_MiniTable_GetArray(msg, repeated_int32_field));
+  protobuf_test_messages_proto2_TestAllTypesProto2_resize_repeated_int32(
+      msg, 10, arena);
+  int32_t* mutable_values =
+      protobuf_test_messages_proto2_TestAllTypesProto2_mutable_repeated_int32(
+          msg, &len);
+  mutable_values[5] = 123;
+  EXPECT_EQ(123, upb_MiniTable_GetArrayValue(
+                     upb_MiniTable_GetArray(msg, repeated_int32_field), 5)
+                     .int32_val);
+  upb_FieldValue new_value;
+  new_value.int32_val = 567;
+  upb_MiniTable_SetArrayValue(upb_MiniTable_GetArray(msg, repeated_int32_field),
+                              5, &new_value);
+  EXPECT_EQ(new_value.int32_val,
+            protobuf_test_messages_proto2_TestAllTypesProto2_repeated_int32(
+                msg, &len)[5]);
+
+  // Test resize.
+  upb_MiniTable_ResizeArray(msg, repeated_int32_field, 20, arena);
+  upb_MiniTable_SetArrayValue(upb_MiniTable_GetArray(msg, repeated_int32_field),
+                              19, &new_value);
+  EXPECT_EQ(new_value.int32_val,
+            protobuf_test_messages_proto2_TestAllTypesProto2_repeated_int32(
+                msg, &len)[19]);
+  upb_MiniTable_ResizeArray(msg, repeated_int32_field, 0, arena);
+  const int32_t* zero_length_array =
+      protobuf_test_messages_proto2_TestAllTypesProto2_repeated_int32(msg,
+                                                                      &len);
+  EXPECT_EQ(0, len);
+  EXPECT_EQ(true, zero_length_array != NULL);
 }
 
 }  // namespace
