6f1fd39c8006429883c11f9482cedef232e149a2
Jason Lunn <jason.lunn@gmail.com>
2023-07-10 09:31:37 -0400

    Expose methods needed by Ruby FFI using UPB_API_INLINE.

--- upb/reflection/def_pool.h
+++ upb/reflection/def_pool.h
@@ -42,17 +42,19 @@
 extern "C" {
 #endif
 
-void upb_DefPool_Free(upb_DefPool* s);
+UPB_API_INLINE void upb_DefPool_Free(upb_DefPool* s);
 
-upb_DefPool* upb_DefPool_New(void);
+UPB_API_INLINE upb_DefPool* upb_DefPool_New(void);
 
-const upb_MessageDef* upb_DefPool_FindMessageByName(const upb_DefPool* s,
+UPB_API_INLINE const upb_MessageDef* upb_DefPool_FindMessageByName(
+                                                    const upb_DefPool* s,
                                                     const char* sym);
 
 const upb_MessageDef* upb_DefPool_FindMessageByNameWithSize(
     const upb_DefPool* s, const char* sym, size_t len);
 
-const upb_EnumDef* upb_DefPool_FindEnumByName(const upb_DefPool* s,
+UPB_API_INLINE const upb_EnumDef* upb_DefPool_FindEnumByName(
+                                              const upb_DefPool* s,
                                               const char* sym);
 
 const upb_EnumValueDef* upb_DefPool_FindEnumByNameval(const upb_DefPool* s,
@@ -87,7 +89,7 @@ const upb_ServiceDef* upb_DefPool_FindServiceByNameWithSize(
 const upb_FileDef* upb_DefPool_FindFileContainingSymbol(const upb_DefPool* s,
                                                         const char* name);
 
-const upb_FileDef* upb_DefPool_AddFile(upb_DefPool* s,
+UPB_API_INLINE const upb_FileDef* upb_DefPool_AddFile(upb_DefPool* s,
                                        const UPB_DESC(FileDescriptorProto) *
                                            file_proto,
                                        upb_Status* status);
